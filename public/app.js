8effd18618c5f6aa9393845b328ceb8f9dd7dfba
Joe Thor <joethor1989@gmail.com>
2018-09-09 07:19:31 -0500

    added ids to handlebars to utilize code already written on the app.js. Updated handlebars to use app.js. Also added a blank style sheet.

--- public/app.js
+++ public/app.js
@@ -9,7 +9,7 @@ $.getJSON("/articles", function(data) {
 
 
 // Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
+$(document).on("click", "div", function() {
   // Empty the notes from the note section
   $("#notes").empty();
   // Save the id from the p tag
