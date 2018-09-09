9451d9bcc8d4d718331a12d7bf6088d866629938
Joe Thor <joethor1989@gmail.com>
2018-09-09 08:37:40 -0500

    only one note div is displayed now and the notes are being saved by their article (need to do more testing, but sure seems that way)

--- public/app.js
+++ public/app.js
@@ -9,7 +9,7 @@ $.getJSON("/articles", function(data) {
 
 
 // Whenever someone clicks a p tag
-$(document).on("click", "div", function() {
+$(document).on("click", ".articleDiv", function() {
   // Empty the notes from the note section
   $("#notes").empty();
   // Save the id from the p tag
@@ -24,7 +24,7 @@ $(document).on("click", "div", function() {
     .then(function(data) {
       console.log(data);
       // The title of the article
-      $("#notes").append("<h2>" + data.title + "</h2>");
+      $("#notes").append("<h2>" + data.titleInfo + "</h2>");
       // An input to enter a new title
       $("#notes").append("<input id='titleinput' name='title' >");
       // A textarea to add a new note body
