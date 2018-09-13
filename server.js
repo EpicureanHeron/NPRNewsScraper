5ca7091188104b72da8998a58a930669033f1375
Joe Thor <joethor1989@gmail.com>
2018-09-12 19:26:21 -0500

    server updated with heroku info

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/NPR";
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
+
 // mongoose.connect("mongodb://localhost/NPR");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
