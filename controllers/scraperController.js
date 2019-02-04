400619ed52af0cb3a54783ddfda80c1284308b48
Joe Thor <joethor1989@gmail.com>
2019-02-03 19:28:26 -0600

    added the requirements back to the scraperController file

--- controllers/scraperController.js
+++ controllers/scraperController.js
@@ -1,3 +1,9 @@
+var request = require("request")
+var cheerio = require("cheerio");
+var mongoose = require("mongoose");
+
+var db = require("../models");
+
 module.exports = function (app) {
 
   app.get("/", function (req, res) {
